03bdc37f5fd5ad5b05a882df5f704d619b9bec2f
Fabiana2903 <149669704+Fabiana2903@users.noreply.github.com>
2024-01-11 16:11:40 +0100

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,6 +1,13 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def print_with_timestamp(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}] {message}")
 
 
 # Start the browser and login with standard_user
@@ -13,6 +20,13 @@ def login (user, password):
     driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
+    product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
+    assert "something" in product_label
+    return driver
 
 login('standard_user', 'secret_sauce')
 
