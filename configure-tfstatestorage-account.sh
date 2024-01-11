f244413b64f2426ec18dc88eb1f86f2ff32d115f
Fabiana2903 <149669704+Fabiana2903@users.noreply.github.com>
2024-01-11 18:59:47 +0100

    Create configure-tfstatestorage-account.sh

---
+++ configure-tfstatestorage-account.sh
@@ -0,0 +1,10 @@
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME"
+echo "STORAGE_ACCOUNT_NAME=$STORAGE_ACCOUNT_NAME"
+echo "CONTAINER_NAME=$CONTAINER_NAME"
+echo "ACCOUNT_KEY=$ACCOUNT_KEY"
+
